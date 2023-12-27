2ff7215641e1504f865159e29e3ce3d6f58d3dd4
AJINKYA <ajinkyacodes@gmail.com>
2023-12-28 04:37:35 +0530

    Moved getUser and getUserRepos components to GithubActions

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -6,7 +6,7 @@ import { searchUsers } from "../../context/github/GithubActions";
 function UserSearch() {
   const [text, setText] = useState("");
 
-  const { users, dispatch, clearUsers } = useContext(GithubContext);
+  const { users, dispatch } = useContext(GithubContext);
 
   const { setAlert } = useContext(AlertContext);
 
@@ -17,7 +17,7 @@ function UserSearch() {
     if (text === "") {
       setAlert("Please enter something", "error");
     } else {
-      dispatch({type: 'SET_LOADING'});
+      dispatch({ type: "SET_LOADING" });
       const users = await searchUsers(text);
       dispatch({ type: "GET_USERS", payload: users });
       setText("");
@@ -51,7 +51,7 @@ function UserSearch() {
         <div>
           <button
             className="btn btn-ghost btn-lg uppercase"
-            onClick={clearUsers}
+            onClick={() => dispatch({ type: "CLEAR_USERS" })}
           >
             Clear
           </button>
