e4106797490e933573459201708ea08ad837eabc
AJINKYA <ajinkyacodes@gmail.com>
2023-12-27 23:40:33 +0530

    created userResults component and fetched github users basic data like usernames and put in grid on ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import UserResults from "../components/users/UserResults";
+
 function Home() {
   return (
-    <div>Home</div>
+    <>        
+        {/* SEARCH COMPONENT */}
+        <UserResults />
+    </>
   )
 }
 
